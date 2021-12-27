647070b90c8aa3186977bcc967573ee1024db2f2
Roman <diggabeat@gmail.com>
2021-12-27 07:37:38 +0300

    first commit

--- src/App.js
+++ src/App.js
@@ -1,24 +1,19 @@
-import logo from './logo.svg';
-import './App.css';
+import React from "react";
+import FormInput from "./components/FormInput";
+import TodoList from "./components/TodoList";
+import Footer from "./components/Footer";
+import { DataProvider } from "./components/DataProvider";
 
 function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
+    <DataProvider>
+      <div className="App">
+        <h1>To do app </h1>
+        <FormInput />
+        <TodoList />
+        <Footer />
+      </div>
+    </DataProvider>
   );
 }
 
